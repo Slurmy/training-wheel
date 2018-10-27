bd05bc9a6ca0e680e00e3f123c66fce808f59c0b
Mornsta <39851171+Mornsta@users.noreply.github.com>
2018-10-27 21:52:24 +0800

    Add files via upload

--- warr-chains.js
+++ warr-chains.js
@@ -1,36 +1,36 @@
 module.exports = [
-    {edge: 10,		skills: ['as'],}
-    {edge: 10,		skills: ['scythe'],}
-    {edge: 9,		skills: ['as'],}
-    {edge: 9,		skills: ['cs', 'tc', 'roll', 'scythe'],}
-    {edge: 9,		skills: ['chargingslash', 'roll', 'scythe'],}
-    {edge: 9,		skills: ['tob', 'roll', 'scythe'],}
-	{edge: 9,		skills: ['rf', 'roll', 'scythe'],}
-	{edge: 9,		skills: ['scythe'],}
-    {edge: 8,		skills: ['as'],}
-    {edge: 8,		skills: ['pb', 'scythe'],}
-	{edge: 8,		skills: ['cs', 'rob', 'scythe'],}
-	{edge: 8,		skills: ['cs', 'rs', 'roll', 'scythe'],}
-	{edge: 7,		skills: ['cs', 'as'],}
-	{edge: 7,		skills: ['chargingslash', 'as'],}
-	{edge: 7,		skills: ['tob', 'as'],}
-	{edge: 7,		skills: ['rs', 'as'],}
-	{edge: 7,		skills: ['rf', 'as'],}
-	{edge: 7,		skills: ['bw', 'bd'],}
-	{edge: 7,		skills: ['chargingslash', 'bd'],}
-	{edge: 7,		skills: ['cs', 'tc', 'bd'],}
-	{edge: 7,		skills: ['rf', 'bd'],}
-	{edge: 7,		skills: ['pb'],}
-	{edge: 7,		skills: ['cs', 'rob'],}
-	{edge: 7,		skills: ['dfa'],}
-	{edge: 7,		skills: ['bs'],}
-     edge           skills
-    {edge: null,	skills: ['bw', 'bd'],}
-    {edge: null,	skills: ['chargingslash', 'bd'],}
-	{edge: null,	skills: ['cs', 'tc', 'bd'],}
-	{edge: null,	skills: ['rf', 'bd'],}
-	{edge: null,	skills: ['pb'],}
-	{edge: null,	skills: ['cs', 'rob'],}
-	{edge: null,	skills: ['dfa'],}
-	{edge: null,	skills: ['bs'],}
+    {edge: 10, skills: ['as'],}
+    {edge: 10, skills: ['scythe'],}
+    {edge: 9, skills: ['as'],}
+    {edge: 9, skills: ['cs', 'tc', 'roll', 'scythe'],}
+    {edge: 9, skills: ['chargingslash', 'roll', 'scythe'],}
+    {edge: 9, skills: ['tob', 'roll', 'scythe'],}
+	{edge: 9, skills: ['rf', 'roll', 'scythe'],}
+	{edge: 9, skills: ['scythe'],}
+    {edge: 8, skills: ['as'],}
+    {edge: 8, skills: ['pb', 'scythe'],}
+	{edge: 8, skills: ['cs', 'rob', 'scythe'],}
+	{edge: 8, skills: ['cs', 'rs', 'roll', 'scythe'],}
+	{edge: 7, skills: ['cs', 'as'],}
+	{edge: 7, skills: ['chargingslash', 'as'],}
+	{edge: 7, skills: ['tob', 'as'],}
+	{edge: 7, skills: ['rs', 'as'],}
+	{edge: 7, skills: ['rf', 'as'],}
+	{edge: 7, skills: ['bw', 'bd'],}
+	{edge: 7, skills: ['chargingslash', 'bd'],}
+	{edge: 7, skills: ['cs', 'tc', 'bd'],}
+	{edge: 7, skills: ['rf', 'bd'],}
+	{edge: 7, skills: ['pb'],}
+	{edge: 7, skills: ['cs', 'rob'],}
+	{edge: 7, skills: ['dfa'],}
+	{edge: 7, skills: ['bs'],}
+	
+    {edge: null, skills: ['bw', 'bd'],}
+    {edge: null, skills: ['chargingslash', 'bd'],}
+	{edge: null, skills: ['cs', 'tc', 'bd'],}
+	{edge: null, skills: ['rf', 'bd'],}
+	{edge: null, skills: ['pb'],}
+	{edge: null, skills: ['cs', 'rob'],}
+	{edge: null, skills: ['dfa'],}
+	{edge: null, skills: ['bs'],}
 ];
